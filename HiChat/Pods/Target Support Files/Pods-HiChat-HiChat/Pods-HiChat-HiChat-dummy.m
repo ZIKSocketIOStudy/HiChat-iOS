6a0c4c9691840a24cc770043a18fa2382a25802b
ZacharyIcoder <811528603@qq.com>
2019-01-07 08:28:33 +0800

    zikong 初始化项目ff

---
+++ HiChat/Pods/Target Support Files/Pods-HiChat-HiChat/Pods-HiChat-HiChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HiChat_HiChat : NSObject
+@end
+@implementation PodsDummy_Pods_HiChat_HiChat
+@end
---
+++ HiChat/Pods/Target Support Files/Pods-HiChat-HiChatKit/Pods-HiChat-HiChatKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HiChat_HiChatKit : NSObject
+@end
+@implementation PodsDummy_Pods_HiChat_HiChatKit
+@end
